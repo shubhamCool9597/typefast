9294e0db2ebf353e78da35e7417812eb28a5a854
Shubham Verma <shubhamcool8527@gmail.com>
2017-07-07 03:15:11 +0530

    wiki

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
-// $('#searchText').typefast({
-// urlstart:"https://en.wikipedia.org/w/api.php?action=opensearch&search=",
-// urlend:"&format=json&callback=?",
-// textHighlight:true,
-// mouseEvent:true,
-// keyboardEvent:true,
-// autocomplete:true
-// });
-
 $('#searchText').typefast({
-urlstart:"http://35.154.56.172/api/project-search/Gurgaon/",
-urlend:"/Flats",
+urlstart:"https://en.wikipedia.org/w/api.php?action=opensearch&search=",
+urlend:"&format=json&callback=?",
+textHighlight:true,
 mouseEvent:true,
 keyboardEvent:true,
-autocomplete:true,
-textHighlight:true,
-keyArr:['data'],
-key:'name'
+autocomplete:true
 });
+
+// $('#searchText').typefast({
+// urlstart:"http://35.154.56.172/api/project-search/Gurgaon/",
+// urlend:"/Flats",
+// mouseEvent:true,
+// keyboardEvent:true,
+// autocomplete:true,
+// textHighlight:true,
+// keyArr:['data'],
+// key:'name'
+// });
 })
